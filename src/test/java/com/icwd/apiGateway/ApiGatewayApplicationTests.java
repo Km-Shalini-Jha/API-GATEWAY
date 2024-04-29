7c5793d97bf7736ab17dcae4a09ab40f04978a17
shalini jha <shalinijha414@gmail.com>
2024-04-30 01:08:38 +0530

    API GATEWAY implementation

---
+++ src/test/java/com/icwd/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.icwd.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
